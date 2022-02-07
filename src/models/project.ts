15c1e9e1eee7b78cb1eda3cdaa438c195e5dbe65
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2022-02-07 10:16:19 -0500

    using es5 modules

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,17 +1,16 @@
 // Project model
-namespace App {
-  export enum ProjectStatus {
-    Active,
-    Finished,
-  }
 
-  export class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) {}
-  }
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
 }
